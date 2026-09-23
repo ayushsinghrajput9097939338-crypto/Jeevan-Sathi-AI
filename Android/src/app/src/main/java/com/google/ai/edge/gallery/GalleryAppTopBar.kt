47ef30eb02b87da4cb79715649f2297450f417db
ayushsinghrajput9097939338-crypto <ayushsinghrajput9097939338@gmail.com>
2026-09-23 06:35:26 +0530

    Update GalleryAppTopBar.kt

--- Android/src/app/src/main/java/com/google/ai/edge/gallery/GalleryAppTopBar.kt
+++ Android/src/app/src/main/java/com/google/ai/edge/gallery/GalleryAppTopBar.kt
@@ -68,7 +68,7 @@ fun GalleryTopAppBar(
         ) {
           if (title == stringResource(R.string.app_name)) {
             Icon(
-              painterResource(R.drawable.logo),
+              painterResource(R.drawable.jeevan_sathi_icon),
               modifier = Modifier.size(20.dp),
               contentDescription = null,
               tint = Color.Unspecified,
